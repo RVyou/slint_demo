d3fa6e461bc7ece87dbd65f137db9d9200a1ae27
rvyou <rvyou@qq.com>
2026-05-09 15:39:50 +0800

    异步修改应用名字

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ fn main() {
     let g = slint_init();
     g.init_global();
     let w = g.main_window.as_weak();
+    //异步ui和函数调用,只能main导出的变量和函数或者main导出的global
     std::thread::spawn(move || {
         std::thread::sleep(std::time::Duration::from_secs(2));
-
         w.upgrade_in_event_loop(|w| {
-            w.set_aa(SharedString::from("aaaa"));
+            w.global::<AppConfig>()
+                .set_app_name(SharedString::from("new name"));
         })
     });
 
